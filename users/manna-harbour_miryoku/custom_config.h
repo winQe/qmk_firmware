5c70602617074cffaabe6b71b3cea3ae9fa7b9bf
Adin <andhika.bhayangkara@desaysv.com>
2025-01-24 16:22:02 +0800

    Swapped placement of pipe and slash

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -52,11 +52,11 @@ U_NP,              U_NP,              KC_DOT,            KC_0,              KC_M
 #define MIRYOKU_LAYER_SYM \
 KC_GRV,            KC_LABK,           KC_RABK,            KC_DQT,           KC_BSLS,           U_NA,              TD(U_TD_U_BASE),   TD(U_TD_U_EXTRA),  TD(U_TD_U_TAP),    TD(U_TD_BOOT),     \
 KC_EXLM,           KC_MINS,           KC_PLUS,            KC_EQL,           KC_HASH,           U_NA,              KC_LSFT,           KC_LCTL,           KC_LALT,           KC_LGUI,           \
-U_NU,              KC_SLSH,           KC_ASTR,           KC_CIRC,           KC_QUES,           U_NA,              TD(U_TD_U_SYM),    TD(U_TD_U_MOUSE),  KC_ALGR,           U_NA,              \
+U_NU,              KC_PIPE,           KC_ASTR,           KC_CIRC,           KC_QUES,           U_NA,              TD(U_TD_U_SYM),    TD(U_TD_U_MOUSE),  KC_ALGR,           U_NA,              \
 U_NP,              U_NP,              U_NU,              KC_TILD,              U_NA,           U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
 #define MIRYOKU_LAYER_MOUSE \
 TD(U_TD_BOOT),     TD(U_TD_U_TAP),    TD(U_TD_U_EXTRA),  TD(U_TD_U_BASE),   U_NA,              KC_AMPR,           KC_QUOT,           KC_LBRC,           KC_RBRC,             KC_AT,             \
-KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,              KC_PIPE,           KC_COLN,           KC_LPRN,           KC_RPRN,           KC_SCLN,             \
+KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,              KC_SLSH,           KC_COLN,           KC_LPRN,           KC_RPRN,           KC_SCLN,             \
 U_NA,              KC_ALGR,           TD(U_TD_U_SYM),    TD(U_TD_U_MOUSE),  U_NA,              KC_PERC,            KC_DLR,           KC_LCBR,           KC_RCBR,              U_NU,             \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              U_NA,              KC_UNDS,              U_NA,           U_NP,                 U_NP
