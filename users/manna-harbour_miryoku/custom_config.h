3299b96d80f03d55738f21e54aa092a3c74191c5
Adin <andhika.bhayangkara@desaysv.com>
2025-01-27 10:44:04 +0800

    Swapped hash with question mark

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -51,8 +51,8 @@ U_NP,              U_NP,              KC_DOT,            KC_0,              KC_M
 
 #define MIRYOKU_LAYER_SYM \
 KC_GRV,            KC_LABK,           KC_RABK,            KC_DQT,           KC_BSLS,           U_NA,              TD(U_TD_U_BASE),   TD(U_TD_U_EXTRA),  TD(U_TD_U_TAP),    TD(U_TD_BOOT),     \
-KC_EXLM,           KC_MINS,           KC_PLUS,            KC_EQL,           KC_HASH,           U_NA,              KC_LSFT,           KC_LCTL,           KC_LALT,           KC_LGUI,           \
-U_NU,              KC_PIPE,           KC_ASTR,           KC_CIRC,           KC_QUES,           U_NA,              TD(U_TD_U_SYM),    TD(U_TD_U_MOUSE),  KC_ALGR,           U_NA,              \
+KC_EXLM,           KC_MINS,           KC_PLUS,            KC_EQL,           KC_QUES,           U_NA,              KC_LSFT,           KC_LCTL,           KC_LALT,           KC_LGUI,           \
+U_NU,              KC_PIPE,           KC_ASTR,           KC_CIRC,           KC_HASH,           U_NA,              TD(U_TD_U_SYM),    TD(U_TD_U_MOUSE),  KC_ALGR,           U_NA,              \
 U_NP,              U_NP,              U_NU,              KC_TILD,              U_NA,           U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
 #define MIRYOKU_LAYER_MOUSE \
